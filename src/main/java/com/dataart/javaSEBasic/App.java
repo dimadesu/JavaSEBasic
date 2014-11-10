2783ec56bcc170db37f28a8c2377fbbffe0af25f
Dima Antonov <antonov.dmitriy@gmail.com>
2014-11-10 17:21:08 +0200

    Email regexp

--- src/main/java/com/dataart/javaSEBasic/App.java
+++ src/main/java/com/dataart/javaSEBasic/App.java
@@ -3,6 +3,7 @@ package com.dataart.javaSEBasic;
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
+import java.util.regex.Matcher;
 
 public class App 
 {
@@ -44,6 +45,7 @@ public class App
 			App app = new App ();
 			
 			app.phoneParser(lines, targetPhonesFilePath);
+			app.emailParser(lines, targetEmailsFilePath);
 			
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -110,6 +112,33 @@ public class App
     	ReadWriteTextFileJDK7 readerWriter = new ReadWriteTextFileJDK7();
     	try {
 			readerWriter.writeLargerTextFile(targetPhonesFilePath, phones);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+    
+    private void emailParser (List<String> lines, String targetEmailsFilePath) {
+    	List<String> emails = new ArrayList<String>();
+    	EmailValidator validator = new EmailValidator();
+    	for(String line: lines)
+    	{
+    		Matcher matcher = validator.validate(line);
+    		while(matcher.find())
+			{
+    			String email = matcher.group();
+    			if(!emails.contains(email)){
+    				emails.add(email);
+    			}
+			}
+    	}
+    	System.out.println("Emails collected:");
+    	for(String email: emails)
+		{
+    		System.out.println(email);
+		}
+    	ReadWriteTextFileJDK7 readerWriter = new ReadWriteTextFileJDK7();
+    	try {
+			readerWriter.writeLargerTextFile(targetEmailsFilePath, emails);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
